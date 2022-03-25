674a6e54866e9b591b89160d57b86015f74a9b0c
nelolik <parshikovav@gmail.com>
2022-03-25 09:59:44 +0300

    Removed from git temp files.

--- target/test-classes/fillRecordsTable.sql
+++
@@ -1,8 +0,0 @@
---CREATE SEQUENCE  IF NOT EXISTS records_seq START WITH 1 INCREMENT BY 1;
-
-insert into records (id, userid, record_name)
-    values (nextval('records_sequence'), 1, 'prostrations'),
-           (nextval('records_sequence'), 1, 'dordje sempa'),
-           (nextval('records_sequence'), 2, 'guru yoga'),
-           (nextval('records_sequence'), 3, 'sitdowns'),
-           (nextval('records_sequence'), 3, 'prostrations');
