1f4323cf9bf584122c1fd8beb18fbc3f8650d3e2
nelolik <parshikovav@gmail.com>
2022-03-29 17:32:00 +0300

    Deleted unused imports.

--- src/main/java/com/nelolik/savecountbot/handler/MessageHandlerImpl.java
+++ src/main/java/com/nelolik/savecountbot/handler/MessageHandlerImpl.java
@@ -1,21 +1,12 @@
 package com.nelolik.savecountbot.handler;
 
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
-import java.io.Serializable;
-import java.util.List;
 
 import static com.nelolik.savecountbot.handler.CallbackMessageHandler.ADD_COUNT_BTN_DATA;
 import static com.nelolik.savecountbot.handler.CallbackMessageHandler.CREATE_RECORD_BTN_DATA;
--- src/main/java/com/nelolik/savecountbot/repositroy/CountsRepository.java
+++ src/main/java/com/nelolik/savecountbot/repositroy/CountsRepository.java
@@ -2,7 +2,6 @@ package com.nelolik.savecountbot.repositroy;
 
 import com.nelolik.savecountbot.model.Counts;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
