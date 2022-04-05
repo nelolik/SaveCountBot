7930ef513718bc6a01c7ba7d1afeb7e9c6279157
nelolik <parshikovav@gmail.com>
2022-04-05 14:14:54 +0300

    Removed bot`s name and token from properties file. They are passed throw command line arguments.

--- src/test/java/com/nelolik/savecountbot/TestConfig.java
+++ src/test/java/com/nelolik/savecountbot/TestConfig.java
@@ -2,7 +2,9 @@ package com.nelolik.savecountbot;
 
 
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.test.context.TestPropertySource;
 
 @TestConfiguration
+@TestPropertySource("classpath:test.properties")
 public class TestConfig {
 }
